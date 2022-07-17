d26059b83118ab3a7be0bd3ed84c7645f8f3f102
Kong To <newlight77@gmail.com>
2022-07-17 21:17:40 +0200

    feature: define hitbox for interlacing between player and rocks

--- src/tile.py
+++ src/tile.py
@@ -5,3 +5,4 @@ class Tile(pygame.sprite.Sprite):
 		super().__init__(groups)
 		self.image = pygame.image.load('assets/graphics/test/rock.png').convert_alpha()
 		self.rect = self.image.get_rect(topleft = pos)
+		self.hitbox = self.rect.inflate(0, -10)
